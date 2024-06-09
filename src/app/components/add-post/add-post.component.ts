be1ba9a6f179615d6df710d1198c910e1ce2bf79
kushanz <kushanshanuka@yahoo.com>
2024-06-09 19:19:08 +0530

    add,edit post

--- src/app/components/add-post/add-post.component.ts
+++ src/app/components/add-post/add-post.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { PostInterface } from '../post/state/post.model';
+import { Store } from '@ngrx/store';
+import { Appstate } from './../../app-store/app.state';
+import { addPost } from '../post/state/post.actions';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-post',
@@ -7,6 +13,32 @@ import { Component } from '@angular/core';
   templateUrl: './add-post.component.html',
   styleUrl: './add-post.component.css'
 })
-export class AddPostComponent {
+export class AddPostComponent implements OnInit {
+  postForm!: FormGroup;
 
+  constructor(private store: Store<Appstate>, private router: Router) {}
+
+  ngOnInit() {
+    this.postForm = new FormGroup({
+      title: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
+      description: new FormControl(null, [Validators.required]),
+    });
+  }
+
+  onAddPost() {
+    if (this.postForm.valid) {
+      console.log(this.postForm.value);
+      const post: PostInterface = {
+        id: null,
+        title: this.postForm.value.title,
+        description: this.postForm.value.description,
+      };
+      this.store.dispatch(addPost(post));
+    }
+    // this.postForm.reset();
+    this.router.navigate(['posts']);
+  }
 }
--- src/app/components/post/post.component.ts
+++ src/app/components/post/post.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { getPostState } from './state/post.selectors';
+import { Appstate } from './../../../app/app-store/app.state';
+import { PostInterface } from './state/post.model';
+import { deletePost } from './state/post.actions';
 
 @Component({
   selector: 'app-post',
@@ -7,6 +13,15 @@ import { Component } from '@angular/core';
   templateUrl: './post.component.html',
   styleUrl: './post.component.css'
 })
-export class PostComponent {
+export class PostComponent implements OnInit {
+  public posts$!: Observable<any>;
+  constructor(private store: Store<Appstate>) {}
 
+  ngOnInit() {
+    this.posts$ = this.store.select(getPostState);
+  }
+  deletePost(post:any) {
+    console.log(post);
+    this.store.dispatch(deletePost(post));
+  }
 }
